5476d71859abbbc4b19d4c381518fb7c16bd4a7b
Edgars Grobins <edgars.grobins@gmail.com>
2022-08-08 15:12:44 +0300

    added ids

--- pling_changer_to_top.py
+++ pling_changer_to_top.py
@@ -1,11 +1,15 @@
 import os
 
-input_folder = r'C:\Users\edgars\Documents\RadioSpots\traffic'
-output_folder = r'C:\Users\edgars\Documents\RadioSpots\traffic_top'
+input_folder = r'D:\darbs\IT\Radio\TOP Radio\Traffic\RadioSpots\in'
+output_folder = r'D:\darbs\IT\Radio\TOP Radio\Traffic\RadioSpots\out'
 search_text_intro = "PLING INTRO"
+search_text_id_in = 'SS11'
+search_text_id_out = 'SS99'
 replace_text_intro = "TOPintro"
 search_text_outro = "PLING OUTRO"
 replace_text_outro = "TOPoutro"
+replace_id_in = 'SS12'
+replace_id_out = 'SS100'
 
 for file in os.listdir(input_folder):
     if os.path.isfile(os.path.join(input_folder, file)) and file.endswith(".2"):
@@ -13,8 +17,12 @@ for file in os.listdir(input_folder):
             data = f.read()
             data = data.replace(search_text_intro, replace_text_intro)
             data = data.replace(search_text_outro, replace_text_outro)
+            data = data.replace(search_text_id_in, replace_id_in)
+            data = data.replace(search_text_id_out, replace_id_out)
 
         with open(os.path.join(output_folder, file), 'w') as f:
             f.write(data)
+
+        os.remove(os.path.join(input_folder, file))
 
 print("Done")
